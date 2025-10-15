968d2a844c2890b2de91b8d95eef80d91e5344e4
waynesbrain <175370544+waynesbrain@users.noreply.github.com>
2025-10-15 18:51:07 -0400

    Work on packaging

--- src/types/emoji.ts
+++ src/types/emoji.ts
@@ -1,41 +1,23 @@
 export type EmojiItem = {
-  /**
-   * A unique name of the emoji which will be stored as attribute
-   */
+  /** Unique name of the emoji. */
   name: string;
-  /**
-   * The emoji unicode character
-   */
+  /** The emoji unicode character. */
   emoji?: string;
-  /**
-   * A list of unique shortcodes that are used by input rules to find the emoji
-   */
+  /** Array of unique strings to find the emoji. */
   shortcodes: string[];
-  /**
-   * A list of tags that can help for searching emojis
-   */
+  /** A list of tags that can help for searching emojis. */
   tags?: string[];
-  /**
-   * A name that can help to group emojis
-   */
+  /** A name that can help to group emojis. */
   group?: string;
-  /**
-   * A list of unique emoticons
-   */
+  /** A list of unique ASCII style emoticons. */
   emoticons?: string[];
-  /**
-   * The unicode version the emoji was introduced
-   */
+  /** The unicode version the emoji was introduced. */
   version?: number;
-
   // TODO: Don't store full URLs, store identifiers...
   // /**
   //  * A fallback image if the current system doesn't support the emoji or for custom emojis
   //  */
   // fallbackImage?: string;
-
-  /**
-   * Store some custom data
-   */
+  /** Store some custom data. */
   [key: string]: any;
 };
